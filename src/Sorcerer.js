5a07cdbd35e1d042685889903df5e880392efccd
Bryan Brophy <brybrophy@gmail.com>
2017-02-10 17:49:39 -0800

    Update Sorcerer

--- src/Sorcerer.js
+++ src/Sorcerer.js
@@ -8,45 +8,45 @@ export default class Sorcerer extends React.Component {
 		this.addErrorSrc = this.addErrorSrc.bind(this);
 	}
 
-    addErrorSrc(e) {
-        e.target.src = this.props.errorSrc;
-    }
+	addErrorSrc(e) {
+		e.target.src = this.props.errorSrc;
+	}
 
-    getSrcSet() {
-        const { defaultSrc, maxDevice, minDevice } = this.props;
-        const devices = this.props.devices ? this.props.devices : getDevices();
+	getSrcSet() {
+		const { defaultSrc, maxDevice, minDevice } = this.props;
+		const devices = this.props.devices ? this.props.devices : getDevices();
 		const srcName = defaultSrc.split('.')[0];
 		const srcExt = defaultSrc.split('.')[1];
 
-        let srcSetStr = '';
+		let srcSetStr = '';
 
-        const min = minDevice ? devices[minDevice] : 320;
-        const max = maxDevice ? devices[maxDevice] : 3840;
+		const min = minDevice ? devices[minDevice] : 320;
+		const max = maxDevice ? devices[maxDevice] : 3840;
 
-        for (let device in devices) {
-            if (devices[device] >= min && devices[device] <= max) {
+		for (let device in devices) {
+			if (devices[device] >= min && devices[device] <= max) {
 				if (devices[device] === max) {
 					srcSetStr += `${srcName}_${device}.${srcExt} ${devices[device]}w`;
 				} else {
 					srcSetStr += `${srcName}_${device}.${srcExt} ${devices[device]}w, `;
 				}
-            }
-        }
+			}
+		}
 
-        return srcSetStr;
-    }
+		return srcSetStr;
+	}
 
-    render() {
-        return (
-            <img
-                alt={this.props.alt}
-                className={this.props.className ? this.props.className : ''}
-                src={this.props.defaultSrc}
-                srcSet={this.getSrcSet()}
-                onError={this.props.errorSrc ? this.addErrorSrc : null}
-            />
-        );
-    }
+	render() {
+		return (
+			<img
+				alt={this.props.alt}
+				className={this.props.className ? this.props.className : ''}
+				src={this.props.defaultSrc}
+				srcSet={this.getSrcSet()}
+				onError={this.props.errorSrc ? this.addErrorSrc : null}
+			/>
+		);
+	}
 }
 
 Sorcerer.propTypes = {
