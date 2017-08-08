af423fbb22fceb49a67e8d5cb9c240183c5a8c17
Bryan Brophy <brybrophy@gmail.com>
2017-08-07 23:49:55 -0700

    Change to relative paths in example

--- example/src/example.js
+++ example/src/example.js
@@ -3,22 +3,22 @@ var ReactDOM = require('react-dom');
 var Sorcerer = require('react-sorcerer');
 
 var App = React.createClass({
-	render () {
-		return (
-			<div className="image-container">
-				<Sorcerer
-					alt="sorcerer"
-					className="example-image"
-					srcExt="jpg"
-					srcName="defaultImg"
-					srcPath="images/100"
-					minDevice="mobileSm"
-					maxDevice="desktopXl"
-					optimizedPath="images/100/optimized"
-				/>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div className="image-container">
+        <Sorcerer
+          alt="sorcerer"
+          className="example-image"
+          srcExt="jpg"
+          srcName="defaultImg"
+          srcPath="./images/100"
+          minDevice="mobileSm"
+          maxDevice="desktopXl"
+          optimizedPath="./images/100/optimized"
+        />
+      </div>
+    );
+  }
 });
 
 ReactDOM.render(<App />, document.getElementById('app'));
