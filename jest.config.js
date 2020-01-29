d0782825da015483f82944e9bad8497c603e2daa
Bryan Brophy <brybrophy@gmail.com>
2020-01-28 18:42:38 -0800

    Update test script

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,6 @@
 module.exports = {
-  moduleFileExtensions: ['js', 'jsx', 'json', 'ts', 'tsx'],
   preset: 'ts-jest',
   setupFilesAfterEnv: ['<rootDir>/setupTests.js'],
-  // testEnvironment: 'node',
   transform: {
     '\\.(ts|tsx)$': 'ts-jest',
     '^.+\\.jsx?$': 'babel-jest'
