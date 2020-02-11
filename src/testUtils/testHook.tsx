cba9d87e739a58266026b52cd6679b03fd1e0276
Bryan Brophy <brybrophy@gmail.com>
2020-02-10 20:39:58 -0800

    Update test hook util to remove @ts-ignore comments

--- src/testUtils/testHook.tsx
+++ src/testUtils/testHook.tsx
@@ -5,12 +5,10 @@ export default function testHook<T>(runHook: any): T {
   function HookWrapper() {
     const output = runHook();
 
-    // @ts-ignore
-    return <span output={output} />;
+    return <span data-output={output} />;
   }
 
   const wrapper = shallow(<HookWrapper />);
 
-  // @ts-ignore
-  return wrapper.find('span').props().output;
+  return wrapper.find('span').prop('data-output');
 }
